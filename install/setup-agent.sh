b0070a469b24442db484f769bc1ba5c9fd8fb8a4
Volodymyr <vovikha@gmail.com>
2014-09-21 14:36:27 +0300

    Update setup

--- install/setup-agent.sh
+++ install/setup-agent.sh
@@ -26,4 +26,8 @@ if [ ! -d "$AGENT_DIR" ]; then
 else
     echo "Using agent at ${AGENT_DIR}."
 fi
+
+export DISPLAY=:99
+Xvfb :99 -screen 0 1024x768x24 &
+java -jar /usr/local/lib/selenium/selenium-server-standalone-2.43.1.jar
 $AGENT_DIR/bin/agent.sh run
